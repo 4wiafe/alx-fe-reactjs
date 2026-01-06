7d129c9db91d9506bc3a512df52fdf111c71c448
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-06 13:22:01 +0000

    Import UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
+import UserProfile from "./components/UserProfile";
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      < UserProfile name="Kakashi" age="50" bio="Copy ninja"/>
       <Footer />
     </>
   )
