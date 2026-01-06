c35ca11323cc51edecb5ca377ba4f9908f999952
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-06 12:45:02 +0000

    Import Header, MainContent, and Footer components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 import './App.css'
 
 function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
