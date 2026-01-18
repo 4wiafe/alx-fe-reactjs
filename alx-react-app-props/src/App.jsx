a8650a804beab6f9b402e12a1903e40df0c90b9c
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-18 09:58:43 +0000

    Refactor App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,22 +1,9 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import WelcomeMessage from "./components/WelcomeMessage";
-import UserProfile from "./components/UserProfile";
-import Count from "./components/Counter";
-import './App.css'
+import ProfilePage from './ProfilePage';
 
 function App() {
-  return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      < UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-      <Count />
-      <Footer />
-    </>
-  )
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
 }
 
-export default App
+export default App;
