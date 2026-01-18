f8c5e8087d6e529334838b9dbe5370f4fd5dde88
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-18 12:03:34 +0000

    Import components in App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 import './App.css'
 
 function App() {
