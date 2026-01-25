452f2e082c9357d0e41abab9ba0439fc1ed4b890
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-25 22:11:12 +0000

    Add edit and delete components

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,9 +1,5 @@
 import { useRecipeStore } from './recipeStore';
 
-const EditRecipeForm = () => {};
-
-const DeleteRecipeButton = () => {};
-
   const RecipeDetails = ({ recipeId }) => {
     const recipe = useRecipeStore(state =>
       state.recipes.find(recipe => recipe.id === recipeId)
@@ -13,7 +9,10 @@ const DeleteRecipeButton = () => {};
       <div>
         <h1>{recipe.title}</h1>
         <p>{recipe.description}</p>
-        {/* Render EditRecipeForm and DeleteRecipeButton here */}
+        <EditRecipeForm recipe={recipe} />
+        <DeleteRecipeButton recipeId={recipe.id} />
       </div>
   );
 };
+
+export default RecipeDetails;
