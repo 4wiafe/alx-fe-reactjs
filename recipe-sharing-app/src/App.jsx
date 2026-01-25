527c89ed1d66f54dc572b71bb3fe17826411990a
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-25 22:27:40 +0000

    Fix router issue

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
-
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <RecipeList />
+            <AddRecipeForm />
+          </>
+        }
+      />
+      <Route path="/recipes/:id" element={<RecipeDetails />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
