c555834de0984ad0578cfec25664c9ebd0b83c49
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-25 21:36:31 +0000

    Remove component folder

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import RecipeList from "./components/RecipeList";
-import   AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./RecipeList";
+import   AddRecipeForm from "./AddRecipeForm";
 
 function App() {
 
