8a7f066e9d300a48a1776ea29a31273cd49dfd2a
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-30 12:39:03 +0000

    Modify App.jsx to include basic routing

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./NotFound";
 import "./App.css";
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <header>
+          <h1>GitHub User Search</h1>
+        </header>
+
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
   )
 }
