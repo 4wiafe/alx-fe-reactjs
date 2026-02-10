e449170559b5d0f783eb7cf7c0728246e8af5ec5
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-10 17:51:08 +0000

    Add more styles

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -24,8 +24,9 @@ export default function HomePage() {
 
   return (
     <div>
+      <h1 className="text-center font-extrabold text-gray-950 text-3xl p-4">Recipe Sharing Platform</h1>
       <div className="p-4 flex flex-col gap-3 bg-gray-100">
-        {data.map(item => (<div key={item.id} className="border m-3 rounded p-4 bg-white shadow-md">
+        {data.map(item => (<div key={item.id} className="border mx-auto max-w-5xl rounded p-4 bg-white shadow-md w-60">
           <h3 className="text-lg font-bold text-blue-500 mb-1">{item.title}</h3>
           <p className="text-gray-500 font-light mb-2">{item.summary}</p>
           <div>
