f33571501486bfabca50b6b88e7691ab2c0ae40a
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-10 17:54:00 +0000

    Add hover effect to cards

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -26,7 +26,7 @@ export default function HomePage() {
     <div>
       <h1 className="text-center font-extrabold text-gray-950 text-3xl p-4">Recipe Sharing Platform</h1>
       <div className="p-4 flex flex-col gap-3 bg-gray-100">
-        {data.map(item => (<div key={item.id} className="border mx-auto max-w-5xl rounded p-4 bg-white shadow-md w-60">
+        {data.map(item => (<div key={item.id} className="border mx-auto max-w-5xl rounded p-4 bg-white shadow-md w-60 hover:border-blue-500">
           <h3 className="text-lg font-bold text-blue-500 mb-1">{item.title}</h3>
           <p className="text-gray-500 font-light mb-2">{item.summary}</p>
           <div>
