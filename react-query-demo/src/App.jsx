b2f8015b2f4d594ed11aa8a8c54fcc6c2a43cbc6
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-22 10:22:56 +0000

    Create component to fetch data with react query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./components/PostsComponents";
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ react-query-demo/src/components/PostsComponents.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Network response was not ok");
+  return res.json();
+};
+
+function PostsComponent() {
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    "posts", 
+    fetchPosts,
+    {
+      staleTime: 1000 * 60 * 5,
+    }
+  );
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
