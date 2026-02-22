f23d7bd3e87ac1dac86e289a42e357f2dd559417
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-22 11:07:22 +0000

    Refactor home component

--- react-router-advanced/src/components/Home.jsx
+++ react-router-advanced/src/components/Home.jsx
@@ -1,8 +1,34 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
       <h2>Home Page</h2>
-      <p>Welcome to the app</p>
+      <p>Welcome! Navigate to different sections:</p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile/details">Profile Details</Link>
+          </li>
+
+          <li>
+            <Link to="/profile/settings">Profile Settings</Link>
+          </li>
+
+          <li>
+            <Link to="/blog/1">Blog Post 1</Link>
+          </li>
+
+          <li>
+            <Link to="/blog/2">Blog Post 2</Link>
+          </li>
+
+          <li>
+            <Link to="/login">Login Page</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
